6b86dce0d0e097e3b5847cc4ea8baefcc96dce22
mattyjtx <mattgonzales3@yahoo.com>
2022-04-07 01:08:03 -0500

    finished timer and punishment for wrong answer

--- script.js
+++ script.js
@@ -1,103 +1,124 @@
 var startButton = document.getElementById('start-btn');
-var nextButton = document.getElementById('next-btn');
-var questionContainerElement = document.getElementById('question-container');
-var questionElement = document.getElementById('question');
-var answerButtonsElement = document.getElementById('answer-btns');
 
-var randomPickedQuestions, currentQuestionIndex
+var questionContainerEl = document.getElementById('question-container');
+var questionEl = document.getElementById('question');
+var answerButtonsEl = document.getElementById('answer-btns');
+var startContainerEl = document.getElementById('start-container');
+var timerEl = document.getElementById('timer');
+var time = 60;
+var timerInterval;
+
+var currentQuestionIndex = 0;
+
 
 
 startButton.addEventListener('click', beginGame);
+questionContainerEl.addEventListener('click', chooseAnswer);
+
 
 
 function beginGame() {
-console.log(beginGame);
-startButton.classList.add('hide');
-randomPickedQuestions = questions.sort(() => Math.random() - .5);
-currentQuestionIndex = 0;
-questionContainerElement.classList.remove('hide');
-setNextQuestion();
+    questionContainerEl.classList.remove("hide");
+    startContainerEl.classList.add('hide');
+
+    timerInterval = setInterval(function(){
+        time--;
+        timerEl.textContent = time;
+    }, 1000)
+
+    showQuestion();
 
 }
 
-function setNextQuestion() { 
-    resetState();
-    showQuestion(randomPickedQuestions[currentQuestionIndex]);
+function finishGame() { 
+  questionContainerEl.innerHTML = "game over :o";
+  clearInterval(timerInterval);
 
 }
 
-function showQuestion(question){
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', chooseAnswer);
-        answerButtonsElement.appendChild(button);
-
-
-
-    });
-}
-
-function resetState() {
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+function showQuestion(){
+    // questionElement.innerText = question.question;
+    // question.answers.forEach(answer => {
+    //     var button = document.createElement('button')
+    //     button.innerText = answer.text
+    //     button.classList.add('btn')
+    //     if (answer.correct) {
+    //         button.dataset.correct = answer.correct
+    //     }
+    //     button.addEventListener('click', chooseAnswer);
+    //     answerButtonsElement.appendChild(button);
+    if (currentQuestionIndex === questions.length) {
+        finishGame();
     }
+
+
+    // });
+    var quizQuestion = document.createElement('p');
+    quizQuestion.textContent= questions[currentQuestionIndex].question;
+    var choicesList = document.createElement('ol');
+    for (var i = 0; i < questions[currentQuestionIndex].choices.length; i++) {
+        var currentChoice = document.createElement('li');
+        currentChoice.textContent = questions[currentQuestionIndex].choices[i];
+        choicesList.append(currentChoice);
+    }
+    questionContainerEl.append(quizQuestion);
+    questionContainerEl.append(choicesList);
+
+    
 }
 
-function chooseAnswer(e) {
 
+
+function chooseAnswer(event) {
+   var currentAnswer = event.target.textContent;
+   var correctAnswer = document.createElement('p');
+   if (currentAnswer === questions[currentQuestionIndex].answer) {
+    
+    correctAnswer.textContent = "correct ;)";
+    timerEl.append(correctAnswer);
+   } else {
+       correctAnswer.textContent = "wrong :(";
+       time -= 5;
+       timerEl.append(correctAnswer);
+   }
+    questionContainerEl.innerHTML = "";
+   currentQuestionIndex++;
+   showQuestion();
+   
 }
+
+
+
 
 var questions = [
     {
         question: 'Commonly used datatypes do NOT include:',
-        answers: [
-            {text: 'strings', correct: false},
-            {text: 'booleans', correct: false },
-            {text: 'alerts', correct: true},
-            {text: 'numbers', correct: false},
-        ]
+        choices: ['strings','booleans', 'alerts', 'numbers'],
+        answer: 'alerts'
     },
+
     {
         question: 'The condition in an if/else statement is enclosed within ___.' ,
-        answers: [
-            {text: 'quotes', correct: false},
-            {text: 'curly brackets', correct: false},
-            {text: 'parenthesis', correct: true},
-            {text: 'square brackets', correct: false},
-        ]
+        choices: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'] ,
+        answer: 'curly brackets'
+        
     },
     {
         question: 'Arrays in Javascript can be used to store ____.' ,
-        answers: [
-            {text: 'numbers & strings', correct: false},
-            {text: 'other arrays', correct: false},
-            {text: 'booleans', correct: true},
-            {text: 'all of the above', correct: false},
-        ]
+        choices: ['numbers & strings', 'other arrays', 'booleans', 'all of the above'] ,
+        answer: 'all of the above'
+        
     },
     {
         question: 'String values must be enclosed with ___ when being assigned to variables.' ,
-        answers: [
-            {text: 'commas', correct: false},
-            {text: 'curly brackets', correct: false},
-            {text: 'quotes', correct: true},
-            {text: 'parenthesis', correct: false},
-        ]
+        choices: ['commas', 'curly brackets', 'quotes', 'parenthesis'] ,
+        answer: 'quotes'
+        
     },
     {
         question: 'A very useful tool used during development and debugging for printing content to the debugger is:' ,
-        answers: [
-            {text: 'Javascript', correct: false},
-            {text: 'terminal/bash', correct: false},
-            {text: 'for loops', correct: false},
-            {text: 'console log', correct: true},
-        ]
+        choices: ['Javascript', 'terminal/bash', 'for loops', 'console log'] ,
+        answer: 'console log'
+        
     },
 ]
